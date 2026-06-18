c64f32f716fe02211a5f56edf9f906d27c7e5fd8
MaskoMys <maskotics@gmail.com>
2026-06-18 15:02:55 +0100

    Delete Triad-Intellect-1.0.0-beta.2 directory

--- Triad-Intellect-1.0.0-beta.2/functions/api/health.ts
+++
@@ -1,9 +0,0 @@
-import type { PagesFunction } from "../../src/server/cloudflare";
-import { jsonResponse } from "../../src/server/http";
-
-export const onRequestGet: PagesFunction = () =>
-  jsonResponse({
-    ok: true,
-    service: "triad-pages-functions",
-    timestamp: new Date().toISOString(),
-  });
